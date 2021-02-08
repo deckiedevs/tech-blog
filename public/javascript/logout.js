2233069743b421a45f74d0220a73539a3bbbe46b
Mila Decker <milacdecker@gmail.com>
2021-02-07 22:07:19 -0500

    added idle timer logout

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -11,4 +11,21 @@ async function logout() {
     };
 };
 
+// logs out after 30 minutes of idling
+function resetTimer() {
+    let time;
+    clearTimeout(time);
+    time = setTimeout(logout, (1000 * 60 * 30));
+};
+
+function idleTimer() {
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer;
+    window.onclick = resetTimer;
+    window.onscroll = resetTimer;
+    window.onkeypress = resetTimer;
+};
+
+idleTimer();
+
 document.querySelector('#logout').addEventListener('click', logout);
